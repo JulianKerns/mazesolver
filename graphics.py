a11ea92fe67bf944c02d4d41e1f35d88d5b2bcdf
JulianKerns <j.kernstock@gmx.at>
2024-02-06 10:23:30 +0100

    added a new classes Point and Line and we can now draw a line in our Window

--- graphics.py
+++ graphics.py
@@ -8,8 +8,8 @@ class Window:
         self.__root = Tk()
         self.__root.title("Maze")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        canvas = Canvas(self.__root, width = self.width, height=self.height)
-        canvas.pack()
+        self.__canvas = Canvas(self.__root, width = self.width, height=self.height)
+        self.__canvas.pack()
         self.__running = False
         
 
@@ -25,6 +25,25 @@ class Window:
             self.redraw()
         print("window closed...")
 
-
     def close(self):
         self.__running = False
+    
+    def draw_line(self, line, fill_color= "black"):
+        line.draw(self.__canvas,fill_color)
+
+class Point:
+    def __init__(self, pos_x, pos_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color= "black"):
+        canvas.create_line(self.point1.pos_x, self.point1.pos_y,self.point2.pos_x,self.point2.pos_y,
+         fill= fill_color,width=2 )
+        canvas.pack()
+        
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from graphics.py import *
+from graphics import Window , Line, Point
 
 def main():
     win = Window(800,600)
+    point1 = Point(400,400)
+    point2 = Point(100,100)
+    line= Line(point1,point2)
+    win.draw_line(line,"black")
     win.wait_for_close()
 
 main()
